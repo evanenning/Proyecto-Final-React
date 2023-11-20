59ad901453b6a84df5000219f27e49727f73ba19
Eva Nenning <118194409+evanenning@users.noreply.github.com>
2023-11-20 18:49:06 -0300

    Logo

--- LandingPage/src/Components/Header.jsx
+++ LandingPage/src/Components/Header.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 
 export default function Header() {
+    const menuList=[
+        {
+            id:1,
+            title: 'Inicio'
+        },
+        {
+            id:1,
+            title: 'Acerca de nosotros'
+        },
+        {
+            id:1,
+            title: 'Contactanos'
+        },
+    ]
   return (
-    <div>Header</div>
+    <div>
+        <div>
+            <h2>Soporte <span>PSI</span></h2>
+        </div>
+
+    </div>
   )
 }
