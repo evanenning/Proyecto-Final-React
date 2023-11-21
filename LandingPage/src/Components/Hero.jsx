f4d2c16d1b3c28efaad497ee0fd2a76d25561df9
Eva Nenning <118194409+evanenning@users.noreply.github.com>
2023-11-20 21:08:14 -0300

    hero & style

--- LandingPage/src/Components/Hero.jsx
+++ LandingPage/src/Components/Hero.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import './Hero.css';
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div class="GeneralConteiner">
+      <div>
+        <p class="SectionTitle">Podemos ayudarte</p>
+        <p class="SectionSubtitle">Contamos con una amplia red de especialistas</p>
+      
+        <div class="buttons">
+          <button class="btn1">Mas información</button>
+          <button class="btnAlert">Contacto urgente</button>
+        </div>
+        </div>
+        <div class="image">
+          <img src="/public/brain.jpg"></img>
+        </div>
+    </div>
   )
 }
 
